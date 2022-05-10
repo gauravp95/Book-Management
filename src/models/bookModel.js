6171f548b46db1e8ff3ae8fb286cc463ad06f339
gaurav95 <gauravpatil4195@gmail.com>
2022-05-10 12:30:29 +0530

    bookController

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -14,7 +14,7 @@ const bookSchema = new mongoose.Schema({
 
   category:{type: String, required: true, trim: true},
   
-  subcategory:{type: String, required: true, trim: true},
+  subcategory:[{type: String, required: true, trim: true}],
 
   reviews:{type: Number,default: 0, comment:" Holds number of reviews of this book"},
 
@@ -22,7 +22,7 @@ const bookSchema = new mongoose.Schema({
 
   isDeleted:{type: Boolean, default: false},
 
-  releasedAt:{type: Date, required: true}
+  releasedAt:{type: Date, required: true, }
   
 },{ timestamps: true } );
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {userLogin,createUser} = require("../controllers/userController")
+const {createBook} = require("../controllers/bookController")
 
 
 
 router.post("/login", userLogin);
 router.post("/register", createUser);
+router.post("/books", createBook);
 
 module.exports = router;
