332cf3cb37c10c1116005f2d379e5a569e9756a9
urajrishuu <urajrishuu@gmail.com>
2022-05-10 17:00:01 +0530

    Done

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {userLogin,createUser} = require("../controllers/userController")
 const { createBook,getBooks, getBooksBYId, updateBook, deleteBook } = require("../controllers/bookController")
+// const middleware = require('../middleware/middleware')
 
 
 //User routes
@@ -12,7 +13,7 @@ router.post("/register", createUser);
 router.post("/books", createBook);
 router.get("/books", getBooks);
 router.get("/books/:bookId", getBooksBYId);
-router.put("/books/:bookId", updateBook);
+router.put("/books/:bookId",updateBook);
 router.delete("/books/:bookId", deleteBook);
 
 
