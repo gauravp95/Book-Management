92b28aa390e040cd8da8aa330a80bdf658c5e1be
gaurav95 <gauravpatil4195@gmail.com>
2022-05-16 17:37:14 +0530

    changes

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -5,30 +5,24 @@ const authentication = async function (req, res, next) {
         const token = req.header('x-auth-token');
 
         if (!token) {
-            return res.status(403).send({ status: false, message: `Missing authentication token in request` });
+            return res.status(403).send({ status: false, message: 'Token must be present'});
         }
 
-        const decoded = jwt.decode(token );
-        console.log(decoded);
-        
-        if (!decoded) {
-            return res.status(400).send({ status: false, message: "Invalid authentication token in request headers." });
+        const decodedToken = jwt.verify(token, 'Project3');
+        if (!decodedToken) {
+            return res.status(403).send({ status: false, message: 'Invalid authentication token in request' });
         }
-        if (Date.now() > (decoded.exp) * 1000) {
+       
+        if (Date.now() > (decodedToken.exp) * 1000) {
             return res.status(403).send({ status: false, message: "Session expired! Please login again." });
         }
-        const verify = jwt.verify(token, 'Project3')
-        console.log(verify);
-        
-        if (!verify) {
-            return res.status(403).send({ status: false, message: `Invalid authentication token in request` });
-        }
+     
         req.userId = decoded.userId;
         next()
     
     } catch (error) {
         
-        return res.status(400).send({ status: false, message: error })}    
+        return res.status(500).send({ status: false, message: error.message })}    
 }
 
 module.exports = {authentication}
